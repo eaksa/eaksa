e91e23f96c22badfc383e28e49a6adb79902d582
Elang Adhyaksa <adhyaksaelang@gmail.com>
2023-10-15 16:51:45 +0700

    Add Python packages

--- modules/dev/python.nix
+++ modules/dev/python.nix
@@ -1,6 +1,12 @@
 { pkgs, ... }: let
   pythonPackages = ps: with ps; [
+    flask
     jupyter
+    pillow
+    pymongo
+    matplotlib
+    numpy
+    replicate
   ];
 in {
   environment.systemPackages = [ (pkgs.python3.withPackages pythonPackages) ];
